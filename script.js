429b679d0be7c00ac925940742d04b563c8986c8
Erikka A <114741859+Akkirea@users.noreply.github.com>
2022-11-19 19:41:58 +0800

    Completed Files

---
+++ script.js
@@ -0,0 +1,18 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let boxOneEl = document.getElementById("boxOne-el");
+let boxTwoEl = document.getElementById("boxTwo-el");
+
+
+function generatePass() {
+    boxOneEl.textContent = "";
+    boxTwoEl.textContent = "";
+    for (let i = 0; i < 15; i++) {
+       let firstRandom = Math.floor( Math.random() * characters.length);
+        boxOneEl.textContent += characters[firstRandom];
+        
+       let secondRandom = Math.floor(Math.random()*characters.length);
+        boxTwoEl.textContent += characters[secondRandom];
+    } 
+}
